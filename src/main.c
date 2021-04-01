1d09a21768b314e4e4da99ea1080c38b0181d0ef
Pietro Moro <pietromoro@programmer.net>
2021-04-01 16:39:19 +0200

    Trail Map

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include "defines.h"
 #include "system/clock.h"
+#include "system/memory.h"
 #include "system/logger.h"
 #include "system/window.h"
 #include "system/opengl.h"
@@ -11,8 +12,17 @@
 #define WINDOW_WIDTH (512)
 #define ASPECT_RATIO (16.0f/9.0f)
 
+#define NUM_AGENTS (300)
+#define MOVE_SPEED (30.0f)
+
 u32 CreateQuadVAO();
 u32 CreateQuadProgram();
+u32 Hash(u32 state);
+
+typedef struct agent {
+  f32 x, y;
+  f32 dir;
+} agent;
 
 i32 CALLBACK WinMain(HINSTANCE instance, HINSTANCE previous, LPSTR cmdLine, i32 showCode) {
   logger_initialize();
@@ -34,8 +44,23 @@ i32 CALLBACK WinMain(HINSTANCE instance, HINSTANCE previous, LPSTR cmdLine, i32 
   shader_info computeShaderInfo[] = {
     {GL_COMPUTE_SHADER, "#version 430\n"
         "layout (local_size_x = 1, local_size_y = 1) in;\n"
-        "layout (rgba32f) uniform image2D imgOut;\n"
-        ""
+        "layout (rgba32f) uniform image2D trailMap;\n"
+        "\n"
+        "#define PI 3.1415926535\n"
+        "\n"
+        "uniform float deltaTime;\n"
+        "uniform float moveSpeed;\n"
+        "uniform int agentCount;\n"
+        "\n"
+        "struct agent {\n"
+        "   vec2 pos;\n"
+        "   float dir;\n"
+        "};\n"
+        "\n"
+        "layout(std140, binding = 0) buffer agentsBuf {\n"
+        "   agent agents[];\n"
+        "};\n"
+        "\n"
         "uint hash(uint state) {\n"
         "   state ^= 2747636419u;\n"
         "   state *= 2654435769u;\n"
@@ -45,24 +70,41 @@ i32 CALLBACK WinMain(HINSTANCE instance, HINSTANCE previous, LPSTR cmdLine, i32 
         "   state *= 2654435769u;\n"
         "   return state;\n"
         "}\n"
-        ""
+        "\n"
         "void main()\n"
         "{\n"
         "   vec4 pixel = vec4(0.0, 1.0, 0.0, 1.0);\n"
-        "   ivec2 pixelCoords = ivec2(gl_GlobalInvocationID.xy);\n"
-        "   ivec2 dims = imageSize(imgOut);\n"
+        "   ivec2 pixelCoord = ivec2(gl_GlobalInvocationID.xy);\n"
+        "   ivec2 imageDim = imageSize(trailMap);\n"
+        "   int pixelIndex = pixelCoord.y * imageDim.y + pixelCoord.x;\n"
+        "   if (pixelIndex.x > agentCount) { return; }\n"
         ""
-        "   int pixelIndex = pixelCoords.y * dims.y + pixelCoords.x;\n"
-        "   vec3 val  = vec3(hash(pixelIndex) / 4294967295.0);\n"
-        "   pixel = vec4(val, 1.0);\n"
-        "   imageStore(imgOut, pixelCoords, pixel);\n"
+        "   agent ag = agents[pixelCoord.x];\n"
+        "   uint random = hash(uint(ag.pos.y * imageDim.y + ag.pos.x + hash(pixelIndex.x)));\n"
+        ""
+        "   vec2 dir = vec2(cos(ag.dir), sin(ag.dir));\n"
+        "   vec2 pos = ag.pos + dir * moveSpeed * deltaTime;\n"
+        ""
+        "   if (pos.x < 0 || pos.x >= imageDim.x || pos.y < 0 || pos.y >= imageDim.y) {\n"
+        "      pos.x = min(imageDim.x - 0.01, max(0, pos.x));\n"
+        "      pos.y = min(imageDim.y - 0.01, max(0, pos.y));\n"
+        "      agents[pixelIndex.x].dir = (random / 4294967295.0) * 2 * PI;\n"
+        "   }\n"
+        ""
+        "   agents[pixelIndex.x].pos = pos;\n"
+        "   imageStore(trailMap, ivec2(pos.x, pos.y), vec4(1.0));\n"
         "}\0"
     },
     0
   };
   
   u32 computeProgram = shader_create(computeShaderInfo);
-  u32 outLoc = glGetUniformLocation(computeProgram, "imgOut");
+  u32 outLoc = glGetUniformLocation(computeProgram, "trailMap");
+  u32 deltaTimeLoc = glGetUniformLocation(computeProgram, "deltaTime");
+  u32 agentsLoc = 0;
+  glUseProgram(computeProgram);
+  glUniform1i(glGetUniformLocation(computeProgram, "agentCount"), NUM_AGENTS);
+  glUniform1f(glGetUniformLocation(computeProgram, "moveSpeed"), MOVE_SPEED);
   
   u32 texture = 0;
   { // create the texture
@@ -91,8 +133,24 @@ i32 CALLBACK WinMain(HINSTANCE instance, HINSTANCE previous, LPSTR cmdLine, i32 
     PH_LOG("Max compute shader invocations %i", groupInv);
   }
   
+  u32 agentsBuffer = 0;
+  glGenBuffers(1, &agentsBuffer);
+  agent* agents = (agent*)ph_alloc(sizeof(agent) * NUM_AGENTS);
+  for (u32 i = 0; i < NUM_AGENTS; i++) {
+    agent* current = &agents[i];
+    u32 rand = Hash((memory_index)(void*)current * i);
+    current->x = (f32)WINDOW_WIDTH / 2.0f + (rand % 100);
+    current->y = (f32)WINDOW_HEIGHT / 2.0f - (rand % 100);
+    current->dir = rand % 360 * (3.14159265f / 180.0f);
+  }
+  glBindBuffer(GL_SHADER_STORAGE_BUFFER, agentsBuffer);
+  glBufferData(GL_SHADER_STORAGE_BUFFER, sizeof(agent) * NUM_AGENTS, 0, GL_DYNAMIC_DRAW);
+  glBufferSubData(GL_SHADER_STORAGE_BUFFER, 0, sizeof(agent) * NUM_AGENTS, agents);
+  glBindBufferBase(GL_SHADER_STORAGE_BUFFER, agentsLoc, agentsBuffer);
+  
+  
   glViewport(0, 0, window->width, window->height);
-  glClearColor(1.0f, 0.0f, 1.0f, 0.0f);
+  glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
   
   f64 lastTime;
   clock_start(clock);
@@ -111,9 +169,12 @@ i32 CALLBACK WinMain(HINSTANCE instance, HINSTANCE previous, LPSTR cmdLine, i32 
     
     glUseProgram(computeProgram);
     glBindImageTexture(outLoc, texture, 0, GL_FALSE, 0, GL_WRITE_ONLY, GL_RGBA32F);
+    glUniform1f(deltaTimeLoc, delta);
+    glBindBuffer(GL_SHADER_STORAGE_BUFFER, agentsBuffer);
+    glBindBufferBase(GL_SHADER_STORAGE_BUFFER, agentsLoc, agentsBuffer);
     glDispatchCompute((u32)window->width, (u32)window->height, 1);
     
-    glMemoryBarrier(GL_SHADER_IMAGE_ACCESS_BARRIER_BIT);
+    glMemoryBarrier(GL_SHADER_IMAGE_ACCESS_BARRIER_BIT | GL_SHADER_STORAGE_BARRIER_BIT );
     
     glClear(GL_COLOR_BUFFER_BIT);
     glUseProgram(quadProgram);
@@ -179,4 +240,14 @@ u32 CreateQuadProgram() {
     0
   };
   return shader_create(quadShaders);
+}
+
+u32 Hash(u32 state) {
+  state ^= 2747636419;
+  state *= 2654435769;
+  state ^= state >> 16;
+  state *= 2654435769;
+  state ^= state >> 16;
+  state *= 2654435769;
+  return state;
 }
